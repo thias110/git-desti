e467c379b31acf982ba91b66c847ec18232c4a4e
Mathias.orlnd <mathiasorlandi12@gmail.com>
2025-08-20 22:26:50 +0200

    Commit initial

---
+++ premierprojetpython/first.py
@@ -0,0 +1,30 @@
+print("Hello");
+puissance = 2 ** 2
+
+somme = 4+4
+produit = 4*4
+int =somme+ produit;
+print(int)
+
+print(3*" oui"+" zero")
+
+print("zo" "ro")
+
+st = "bonjour"
+print(st[0])
+print(st[:2])
+
+liste = [1,2,2,4]
+print(liste[0])
+print[liste[-2]]
+
+liste + [12,48,46,15]
+liste[3]=10;
+
+liste.append=10
+
+
+
+
+
+
